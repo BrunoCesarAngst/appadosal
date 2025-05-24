dbe30b552a6f46093a7aff9b6eb64fbd6a7b6c67
BrunoCesarAngst <bruno.angst@rede.ulbra.br>
2025-05-24 17:19:11 -0300

    Remove arquivos de configuração desnecessários, incluindo .npmrc, turbo.json e tipos relacionados, além de refatorar scripts no package.json para utilizar pnpm e melhorar a legibilidade dos tsconfig.json nas aplicações.

--- packages/types/trpc.ts
+++
@@ -1,9 +0,0 @@
-import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
-import type { AppRouter } from "../../apps/server/src/routers";
-
-// Interface pura para o router
-export type { AppRouter };
-
-// Tipos úteis para o cliente
-export type RouterInput = inferRouterInputs<AppRouter>;
-export type RouterOutput = inferRouterOutputs<AppRouter>;
