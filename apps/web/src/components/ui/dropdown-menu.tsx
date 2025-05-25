62f3cb67ce1a255f9df6f093e674625af4bfbbe2
BrunoCesarAngst <bruno.angst@rede.ulbra.br>
2025-05-25 10:21:09 -0300

    refatora o componente DropdownMenu para remover o uso de React.forwardRef e simplificar a tipagem das propriedades

--- apps/web/src/components/ui/dropdown-menu.tsx
+++ apps/web/src/components/ui/dropdown-menu.tsx
@@ -1,15 +1,12 @@
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
 import { CheckIcon, ChevronRightIcon, CircleIcon } from "lucide-react";
-import * as React from "react";
+import type * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-const DropdownMenu = React.forwardRef<
-	React.ElementRef<typeof DropdownMenuPrimitive.Root>,
-	React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Root>
->(function DropdownMenu(props, ref) {
-	return <DropdownMenuPrimitive.Root ref={ref} data-slot="dropdown-menu" {...props} />;
-});
+function DropdownMenu(props: React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Root>) {
+	return <DropdownMenuPrimitive.Root data-slot="dropdown-menu" {...props} />;
+}
 
 function DropdownMenuPortal({
 	...props
