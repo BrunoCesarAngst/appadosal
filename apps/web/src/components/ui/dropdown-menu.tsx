24444f96d820f53420a5136546206101db1309b2
BrunoCesarAngst <bruno.angst@rede.ulbra.br>
2025-05-25 10:54:52 -0300

    refatora o componente DropdownMenu para corrigir a importação do React e remover uma linha em branco desnecessária

--- apps/web/src/components/ui/dropdown-menu.tsx
+++ apps/web/src/components/ui/dropdown-menu.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { cn } from "@/lib/utils";
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
 import { CheckIcon, ChevronRightIcon, CircleIcon } from "lucide-react";
